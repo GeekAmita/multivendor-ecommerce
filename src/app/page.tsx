8d922c66be2ebff96fd5ca9d24a5ead60d754ff0
GeekAmita <amitasingh5661@gmail.com>
2026-04-02 08:17:31 +0530

    style: update UI component aesthetics, typography, and theme variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
-    <div>
-      <p className="text-amber-700">Welcome</p>
+    <div className="flex flex-col gap-y-6 p-6">
+      <div>
+        <Button variant={"elevated"}>Click me</Button>
+      </div>
+      <div>
+        <Input placeholder="Enter your name" />
+      </div>
+      <div>
+        <Progress value={50} />
+      </div>
+      <div>
+        <Textarea placeholder="Enter your message" />
+      </div>
+      <div>
+        <Checkbox />
+      </div>
     </div>
   );
 }
